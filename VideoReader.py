d4f55f7a96751ee1b8ebc8944183d0d630b1076a
tverdokn <kolyatverdokhleb@gmail.com>
2022-01-23 00:16:38 +0100

    Add independent frame processing to VideoReader

--- VideoReader.py
+++ VideoReader.py
@@ -41,15 +41,25 @@ class VideoReader:
         self.frame_width = int(self.cap.get(3))
         self.frame_height = int(self.cap.get(4))
 
+    def get_stream_params(self):
+        return (self.amount_of_frames, self.fps, self.frame_width, self.frame_height)
+
     def set_ofstream(self, mode, write_to):
-        if mode == "write_mp4":
+        if mode == "write_mp4_grayscale":
+            fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+            self.out = cv2.VideoWriter(write_to, fourcc, self.fps, (self.frame_width, self.frame_height), 0)
+        elif mode == "write_mp4":
             fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
             self.out = cv2.VideoWriter(write_to, fourcc, self.fps, (self.frame_width, self.frame_height))
         elif "write_txt":
             self.out = open(write_to, 'a')
 
+    def release_instream(self):
+        self.cap.release()
+
     def release_ofstream(self, mode):
-        if mode == "write_mp4":
+        self.cap.release()
+        if mode == "write_mp4" or mode == "write_mp4_grayscale":
             self.out.release()
         elif "write_txt":
             self.out.close()
@@ -79,14 +89,14 @@ class VideoReader:
     def process_mp4frame(self):
         print("To be done")
 
-    def process_some_mp4frames(self, read_from, start_frame_ind, end_frame_ind, mode="read", write_to=None):
+    def process_some_mp4frames(self, start_frame_ind, end_frame_ind, mode="read", write_to=None):
         """
         This method reads some (start_frame_ind to end_frame_ind) frames from the mp4 stream
         """
-        self.set_instream(read_from)
-        print('mode = ', mode)
-        if mode == "write_mp4" or mode == "write_txt":
+        if mode == "write_mp4" or mode == "write_mp4_grayscale" or mode == "write_txt":
             self.set_ofstream(mode, write_to)
+        elif mode == "return_grayscale":
+            storage = []
 
         for frame_ind in range(start_frame_ind, end_frame_ind, 1):
             # check for valid frame number
@@ -94,51 +104,52 @@ class VideoReader:
                 # set frame position
                 self.cap.set(int(self.amount_of_frames), frame_ind)
                 ret, frame = self.cap.read()
-                print("WE ret", ret)
                 if ret:
-                    if   mode == "write_mp4":
+                    if mode == "write_mp4" or mode == "write_mp4_grayscale":
                         self.write_frame_mp4(frame)
                     elif mode == "write_txt":
                         self.write_frame_txt(frame)
                     elif mode == "show":
-                        print("WE SHOW")
                         cv2.imshow("frame", frame)
                         cv2.waitKey(0)
+                    elif mode == "return_grayscale":
+                        storage.append(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
+                        # storage.append(frame)
                     elif mode == "process":
                         print("Processing frame")
                 else:
                     break
+        if mode == "return_grayscale":
+            return storage
 
-        self.cap.release()
-        if mode == "write_mp4" or mode == "write_txt":
-            self.release_ofstream(mode)
-
-    def process_all_mp4frames(self, read_from, mode="read", write_to=None):
+    def process_all_mp4frames(self, mode="read", write_to=None):
         """
         This method reads all the frames from the mp4 stream
         """
-        self.set_instream(read_from)
-        if mode == "write_mp4" or mode == "write_txt":
+        if mode == "write_mp4" or mode == "write_mp4_grayscale" or mode == "write_txt":
             self.set_ofstream(mode, write_to)
+        elif mode == "return_grayscale":
+            storage = []
 
         frame_index = 0
         while self.cap.isOpened():
             print("Frame index = ", frame_index)
             ret, frame = self.cap.read()
             if ret:
-                if   mode == "write_mp4":
+                if mode == "write_mp4" or mode == "write_mp4_grayscale":
                     self.write_frame_mp4(frame)
                 elif mode == "write_txt":
                     self.write_frame_txt(frame)
                 elif mode == "show":
                     cv2.imshow("frame", frame)
                     cv2.waitKey(0)
+                elif mode == "return_grayscale":
+                    storage.append(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
                 elif mode == "process":
                     print("Processing frame")
             else:
                 break
             frame_index += 1
 
-        self.cap.release()
-        if mode == "write_mp4" or mode == "write_txt":
-            self.release_ofstream(mode)
+        if mode == "return_grayscale":
+            return storage
